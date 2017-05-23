5ef0327f907b624299658abac7270c244bb765fe
William F. Broderick <billbrod@gmail.com>
2017-05-23 00:14:02 -0400

    Updates GPyr to pythonic

--- pyPyrTools/Gpyr.py
+++ pyPyrTools/Gpyr.py
@@ -10,46 +10,32 @@ class Gpyr(Lpyr):
     height = ''
 
     # constructor
-    def __init__(self, *args):    # (image, height, filter, edges)
+    def __init__(self, image, height='auto', filt='binom5', edges='reflect1'):
         self.pyrType = 'Gaussian'
-        if len(args) < 1:
-            print "pyr = Gpyr(image, height, filter, edges)"
-            print "First argument (image) is required"
-            return
-        else:
-            self.image = args[0]
+        self.image = image
 
-        if len(args) > 2:
-            filt = args[2]
-            if not (filt.shape == 1).any():
-                print "Error: filt should be a 1D filter (i.e., a vector)"
-                return
+        if isinstance(filt, basestring):
+            self.filt = namedFilter(filt)
         else:
-            print "no filter set, so filter is binom5"
-            filt = namedFilter('binom5')
-            if self.image.shape[0] == 1:
-                filt = filt.reshape(1,5)
-            else:
-                filt = filt.reshape(5,1)
+            self.filt = filt
+        if not (numpy.array(self.filt.shape) == 1).any():
+            raise Exception("filt should be a 1D filter (i.e., a vector)")
 
-        maxHeight = 1 + maxPyrHt(self.image.shape, filt.shape)
+        # print self.filt.shape
+        # if self.image.shape[0] == 1:
+        #     self.filt = self.filt.reshape(1, max(self.filt.shape))
+        # print self.filt.shape
+        
+        maxHeight = 1 + maxPyrHt(self.image.shape, self.filt.shape)
 
-        if len(args) > 1:
-            if args[1] is "auto":
-                self.height = maxHeight
-            else:
-                self.height = args[1]
-                if self.height > maxHeight:
-                    print ( "Error: cannot build pyramid higher than %d levels"
-                            % (maxHeight) )
-                    return
-        else:
+        if height == "auto":
             self.height = maxHeight
-
-        if len(args) > 3:
-            edges = args[3]
         else:
-            edges = "reflect1"
+            self.height = height
+            if self.height > maxHeight:
+                raise Exception("Cannot build pyramid higher than %d levels" % (maxHeight))
+
+        self.edges = edges
 
         # make pyramid
         self.pyr = []
@@ -66,19 +52,19 @@ class Gpyr(Lpyr):
 
         for ht in range(self.height-1,0,-1):
             im_sz = im.shape
-            filt_sz = filt.shape
+            filt_sz = self.filt.shape
             if im_sz[0] == 1:
-                lo2 = corrDn(image = im, filt = filt, step = (1,2))
+                lo2 = corrDn(image=im, filt=self.filt, step=(1, 2))
                 #lo2 = numpy.array(lo2)
             elif len(im_sz) == 1 or im_sz[1] == 1:
-                lo2 = corrDn(image = im, filt = filt, step = (2,1))
+                lo2 = corrDn(image=im, filt=self.filt, step=(2, 1))
                 #lo2 = numpy.array(lo2)
             else:
-                lo = corrDn(image = im, filt = filt.T, step = (1,2),
-                            start = (0,0))
+                lo = corrDn(image=im, filt=self.filt.T, step=(1, 2),
+                            start=(0, 0))
                 #lo = numpy.array(lo)
-                lo2 = corrDn(image = lo, filt = filt, step = (2,1),
-                             start = (0,0))
+                lo2 = corrDn(image=lo, filt=self.filt, step=(2, 1),
+                             start=(0, 0))
                 #lo2 = numpy.array(lo2)                
 
             self.pyr.append(lo2.copy())
