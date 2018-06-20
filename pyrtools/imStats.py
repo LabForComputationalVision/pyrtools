c4c6b58f493f7f67924a8046081ed79e1ef47660
Zhuo Wang <zhuowang@172-16-112-41.dynapool.nyu.edu>
2018-06-20 18:06:26 -0400

    split imStats(*arg) into imStats(im_array) and imCompare(im1, im2)

--- pyrtools/imStats.py
+++ pyrtools/imStats.py
@@ -19,7 +19,7 @@ def var2(np_array, mean=None):
     if np.isreal(np_array).all():
         return ((np_array - mean)**2).sum() / max(np_array.size - 1, 1)
     else:
-        return var2(np_array.real, mean.real) + 1j * var2(np_array.imag, mean.image)
+        return var2(np_array.real, mean.real) + 1j * var2(np_array.imag, mean.imag)
 
 def skew2(np_array, mean=None, var=None):
     ''' Sample skew (third moment divided by variance^3/2) of the input numpy array.
@@ -50,46 +50,43 @@ def kurt2(np_array, mean=None, var=None):
     else:
         return kurt2(np_array.real, mean.real, var.real) + 1j * kurt2(np_array.imag, mean.imag, var.imag)
 
-def imStats(*args):
-    ''' Report image (matrix) statistics.
-        When called on a single image IM1, report min, max, mean, stdev,
-        and kurtosis.
-        When called on two images (IM1 and IM2), report min, max, mean,
-        stdev of the difference, and also SNR (relative to IM1).  '''
-
-    if len(args) == 0:
-        print('Error: at least one input image is required')
-        return
-    elif len(args) == 1 and not np.isreal(args[0]).all():
+def imCompare(im_array0, im_array1):
+    ''' Report min, max, mean, stdev of the difference,
+        and SNR (relative to IM1).  '''
+    if not np.isreal(im_array0).all() or not np.isreal(im_array1).all()):
         print('Error: input images must be real-valued matrices')
         return
-    elif len(args) == 2 and ( not np.isreal(args[0]).all() or not np.isreal(args[1]).all()):
-        print('Error: input images must be real-valued matrices')
-        return
-    elif len(args) > 2:
-        print('Error: maximum of two input images allowed')
-        return
 
-    if len(args) == 2:
-        difference = args[0] - args[1]
-        (mn, mx) = range2(difference)
-        mean = difference.mean()
-        v = var2(difference)
-        if v < np.finfo(np.double).tiny:
-            snr = np.inf
-        else:
-            snr = 10 * np.log10(var2(args[0])/v)
-        print('Difference statistics:')
-        print('  Range: [%d, %d]' % (mn, mx))
-        print('  Mean: %f,  Stdev (rmse): %f,  SNR (dB): %f' % (mean,
-                                                                np.sqrt(v),
-                                                                snr))
+    difference = im_array0 - im_array1
+    (min_diff, max_diff) = range2(difference)
+    mean_diff = difference.mean()
+    var_diff = var2(difference, mean)
+    if var_diff < np.finfo(np.double).tiny:
+        snr = np.inf
     else:
-        (mn, mx) = range2(args[0])
-        mean = args[0].mean()
-        var = var2(args[0])
-        stdev = np.sqrt(var.real) + np.sqrt(var.imag)
-        kurt = kurt2(args[0], mean, stdev**2)
-        print('Image statistics:')
-        print('  Range: [%f, %f]' % (mn, mx))
-        print('  Mean: %f,  Stdev: %f,  Kurtosis: %f' % (mean, stdev, kurt))
+        snr = 10 * np.log10(var2(im_array0) / var_diff)
+    print('Difference statistics:')
+    print('  Range: [%d, %d]' % (min_diff, max_diff))
+    print('  Mean: %f,  Stdev (rmse): %f,  SNR (dB): %f' % (mean_diff,
+                                                            np.sqrt(var_diff),
+                                                            snr))
+
+def imStats(im_array):
+    ''' Report image (matrix) statistics: min, max, mean, stdev,
+        and kurtosis.
+        '''
+
+    if not np.isreal(im_array).all():
+        print('Error: input images must be real-valued matrices')
+        return
+
+    (mini, maxi) = range2(im_array)
+    mean = im_array.mean()
+    var = var2(im_array, mean)
+    stdev = np.sqrt(var.real) + np.sqrt(var.imag)
+
+    # why is the kurtosis computed this way for complex matrices? (-Jimmy)
+    kurt = kurt2(im_array, mean, stdev**2)
+    print('Image statistics:')
+    print('  Range: [%f, %f]' % (mini, maxi))
+    print('  Mean: %f,  Stdev: %f,  Kurtosis: %f' % (mean, stdev, kurt))
