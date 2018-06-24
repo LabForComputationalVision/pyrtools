596ae3623a86eb9774f76d710ab63e41021c991e
ravos <jswangzhuo@gmail.com>
2018-06-23 23:44:07 -0400

    bug fixed

--- pyrtools/histoMatch.py
+++ pyrtools/histoMatch.py
@@ -1,9 +1,9 @@
-import numpy
-import scipy
+import numpy as np
+from scipy.interpolate import interp1d
 from .imStats import matlab_histo
 from .convolutions import pointOp
 
-def histoMatch(*args):
+def histoMatch(mtx, N, X, mode='edges'):
     ''' RES = histoMatch(MTX, N, X, mode)
 
         Modify elements of MTX so that normalized histogram matches that
@@ -15,39 +15,26 @@ def histoMatch(*args):
 
         Eero Simoncelli, 7/96. Ported to Python by Rob Young, 10/15.  '''
 
-    mode = str(args[3])
-    mtx = numpy.array(args[0])
-    N = numpy.array(args[1])
-    X = numpy.array(args[2])
-    if mode == 'edges':         # convert to centers
-        correction = (X[0][1] - X[0][0]) / 2.0
-        X = (X[0][:-1] + correction).reshape(1, X.shape[1]-1)
+    [oN, oX] = matlab_histo(mtx, X.size)
+    oStep = oX[0,1] - oX[0,0]
+    oC = np.concatenate((np.array([0]),
+                            np.cumsum(oN / oN.sum())
+                            ))
 
-    [oN, oX] = matlab_histo(mtx.flatten(), X.flatten().shape[0])
-    if mode == 'edges':        # convert to centers
-        correction = (oX[0][1] - oX[0][0]) / 2.0
-        oX = (oX[0][:-1] + correction).reshape(1, oX.shape[1]-1)
+    if mode == 'centers':         # convert to edges
+        nStep = X[0,1] - X[0,0]
+        nX = np.concatenate((np.array([X[0,0] - 0.5 * nStep]),
+                                np.array( X[0,:] + 0.5 * nStep)))
+    else:
+        nX = X.flatten()
 
-    # remember: histo returns a column vector, so the indexing is thus
-    oStep = oX[0][1] - oX[0][0]
-    oC = numpy.concatenate((numpy.array([0]),
-                            numpy.array(numpy.cumsum(oN) /
-                                        float(sum(sum(oN))))))
-    oX = numpy.concatenate((numpy.array([oX[0][0]-oStep/2.0]),
-                            numpy.array(oX[0]+oStep/2.0)))
-
-    N = N.flatten()
-    X = X.flatten()
     N = N + N.mean() / 1e8  # HACK: no empty bins ensures nC strictly monotonic
-
-    nStep = X[1] - X[0]
-    nC = numpy.concatenate((numpy.array([0]),
-                            numpy.array(numpy.cumsum(N) / sum(N))))
-    nX = numpy.concatenate((numpy.array([X[0] - nStep / 2.0]),
-                            numpy.array(X+nStep / 2.0)))
+    nC = np.concatenate((np.array([0]),
+                            np.cumsum(N / N.sum())
+                            ))
 
     # unlike in matlab, interp1d returns a function
-    func = scipy.interpolate.interp1d(nC, nX, 'linear')
+    func = interp1d(nC, nX, 'linear')
     nnX = func(oC)
 
-    return pointOp(mtx, nnX, oX[0], oStep, 0)
+    return pointOp(mtx, nnX, oX[0,0], oStep, 0)
