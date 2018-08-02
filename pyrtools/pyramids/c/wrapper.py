8c131c01906622d117cb00b355d4615dcc98fbf8
William F. Broderick <billbrod@gmail.com>
2018-08-02 14:59:57 -0400

    removes result argument from upConv/corrDn

--- pyrtools/pyramids/c/wrapper.py
+++ pyrtools/pyramids/c/wrapper.py
@@ -17,7 +17,7 @@ libpath = glob.glob(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'w
 lib = ctypes.cdll.LoadLibrary(libpath[0])
 
 
-def corrDn(image, filt, edges='reflect1', step=(1, 1), start=(0, 0), stop=None, result=None):
+def corrDn(image, filt, edges='reflect1', step=(1, 1), start=(0, 0), stop=None):
     """Compute correlation of matrices image with `filt, followed by downsampling.
 
     These arguments should be 1D or 2D matrices, and image must be larger (in both dimensions) than
@@ -56,12 +56,9 @@ def corrDn(image, filt, edges='reflect1', step=(1, 1), start=(0, 0), stop=None, 
     if stop is None:
         stop = (image.shape[0], image.shape[1])
 
-    if result is None:
-        rxsz = len(range(start[0], stop[0], step[0]))
-        rysz = len(range(start[1], stop[1], step[1]))
-        result = np.zeros((rxsz, rysz))
-    else:
-        result = np.array(result.copy())
+    rxsz = len(range(start[0], stop[0], step[0]))
+    rysz = len(range(start[1], stop[1], step[1]))
+    result = np.zeros((rxsz, rysz))
 
     if edges == 'circular':
         lib.internal_wrap_reduce(image.ctypes.data_as(ctypes.POINTER(ctypes.c_double)),
@@ -86,7 +83,7 @@ def corrDn(image, filt, edges='reflect1', step=(1, 1), start=(0, 0), stop=None, 
     return result
 
 
-def upConv(image, filt, edges='reflect1', step=(1, 1), start=(0, 0), stop=None, result=None):
+def upConv(image, filt, edges='reflect1', step=(1, 1), start=(0, 0), stop=None):
     """Upsample matrix image, followed by convolution with matrix filt.
 
     These arguments should be 1D or 2D matrices, and image must be larger (in both dimensions) than
@@ -106,10 +103,6 @@ def upConv(image, filt, edges='reflect1', step=(1, 1), start=(0, 0), stop=None, 
 
     The window over which the convolution occurs is specfied by start (optional, default=(0, 0),
     and stop (optional, default = step .* (size(IM) + floor((start-1)./step))).
-
-    result is an optional result matrix.  The convolution result will be destructively added into
-    this matrix.  If this argument is passed, the result matrix will not be returned. DO NOT USE
-    THIS ARGUMENT IF YOU DO NOT UNDERSTAND WHAT THIS MEANS!!
 
     NOTE: this operation corresponds to multiplication of a signal vector by a matrix whose columns
     contain copies of the time-reversed (or space-reversed) FILT shifted by multiples of STEP.  See
@@ -138,15 +131,10 @@ def upConv(image, filt, edges='reflect1', step=(1, 1), start=(0, 0), stop=None, 
         else:
             raise Exception('Even sized 2D filters not yet supported by upConv.')
 
-    if stop is None and result is None:
+    if stop is None:
         stop = [imshape_d * step_d for imshape_d, step_d in zip(image.shape, step)]
-    elif stop is None:
-        stop = result.shape
 
-    if result is None:
-        result = np.zeros((stop[1], stop[0]))
-    else:
-        result = np.array(result.copy())
+    result = np.zeros((stop[1], stop[0]))
 
     temp = np.zeros((filt.shape[1], filt.shape[0]))
 
