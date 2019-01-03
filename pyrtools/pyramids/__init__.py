971129e714463552b711b2af18f9df3938678b10
William F. Broderick <billbrod@gmail.com>
2019-01-03 13:35:51 -0500

    removes SteerablePyramidOld
    this doesn't need to be in repo, just using for testing

--- pyrtools/pyramids/__init__.py
+++ pyrtools/pyramids/__init__.py
@@ -3,6 +3,5 @@ from .LaplacianPyramid import LaplacianPyramid
 from .WaveletPyramid import WaveletPyramid
 from .SteerablePyramidSpace import SteerablePyramidSpace
 from .SteerablePyramidFreq import SteerablePyramidFreq
-from .SteerablePyramidOld import SteerablePyramidOld
 from .steer import steer, steer2HarmMtx
 from .pyr_utils import LB2idx, idx2LB, convert_pyr_coeffs_to_pyr
