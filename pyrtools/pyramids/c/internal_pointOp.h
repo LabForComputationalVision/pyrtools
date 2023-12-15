c9c8ab7023f645cfcab39448a8a98f8760ba378f
Brian Ward <bward@flatironinstitute.org>
2023-12-15 15:14:16 -0500

    Minimal patch for Windows build

--- pyrtools/pyramids/c/internal_pointOp.h
+++ pyrtools/pyramids/c/internal_pointOp.h
@@ -1,3 +1,5 @@
-void internal_pointop(double *im, double *res, int size, double *lut, 
-		      int lutsize, double origin, double increment, 
-		      int warnings);
+#include "meta.h"
+
+PYRTOOLS_EXPORT void internal_pointop(double *im, double *res, int size, double *lut,
+									  int lutsize, double origin, double increment,
+									  int warnings);
