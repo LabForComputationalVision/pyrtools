d80c62055780a9788163ddc6d5b5fa377a25ca31
Brian Ward <bward@flatironinstitute.org>
2023-12-15 15:22:14 -0500

    Build version-independent wheels, update deploy action to build

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 #! /usr/bin/env python
 
+from wheel.bdist_wheel import bdist_wheel
 from setuptools import setup, Extension
 import importlib
 import os
@@ -10,6 +11,17 @@ pyrtools_version_spec = importlib.util.spec_from_file_location('pyrtools_version
 pyrtools_version_module = importlib.util.module_from_spec(pyrtools_version_spec)
 pyrtools_version_spec.loader.exec_module(pyrtools_version_module)
 VERSION = pyrtools_version_module.version
+
+# Adapted from the cibuildwheel example https://github.com/joerick/python-ctypes-package-sample
+# it marks the wheel as not specific to the Python API version.
+class WheelABINone(bdist_wheel):
+    def finalize_options(self):
+        bdist_wheel.finalize_options(self)
+        self.root_is_pure = False
+
+    def get_tag(self):
+        _, _, plat = bdist_wheel.get_tag(self)
+        return "py3", "none", plat
 
 
 setup(
@@ -40,5 +52,6 @@ setup(
                                     'pyrtools/pyramids/c/convolve.h',
                                     'pyrtools/pyramids/c/internal_pointOp.h'],
                            extra_compile_args=['-fPIC', '-shared'])],
+    cmdclass={"bdist_wheel": WheelABINone},
     tests='TESTS',
     )
